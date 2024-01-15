6e351ae7ea32109709c5deeda0dad9af8100c395
ian30 <ian.schneider37@gmail.com>
2024-01-14 19:07:03 -0700

    adding textbox on click of element

--- script.js
+++ script.js
@@ -26,101 +26,89 @@ editMenu.addEventListener('click', function () {
 });
 
 //test
-const testLink = document.getElementById('testLink');
-testLink.addEventListener('click', function () {
-    createTextInputUnderEl();
-})
+// const testLink = document.getElementById('testLink');
+// testLink.addEventListener('click', function () {
+//     createTextInputUnderEl();
+// })
 
 
-//handling elements drag:
+//workArea listens to element dropped:
 const workArea = document.getElementById('workArea');
 workArea.addEventListener('dragover', function (e) {
     e.preventDefault();
 })
-
+//letting items in the toolbox to be dragged and dropped
 for (const tool of tools) {
     tool.addEventListener('dragstart', function (event) {
-        //console.log(event.target.id);
         event.dataTransfer.setData('text', event.target.id);
     });
 }
-let workElements = document.querySelectorAll('#workArea *');
-setTimeout(function () {
-    workElements = document.querySelectorAll('#workArea *');
-    console.log('workElements: ', workElements);
-}, 12000)
 
-workElements.forEach(element => {
-    element.addEventListener('click', function (event) {
-        console.log('click on', event.target.id);
-    });
-});
 function createTextInputUnderEl(element) {
     let workAreaElements = workArea.querySelectorAll('*');
+    console.log('workAreaElements', workAreaElements);
     workAreaElements.forEach(element => {
+        element.addEventListener('click', function (event) {
+            let child = this.querySelectorAll('span');
+            element.classList.toggle('editing');
+            if (child.length > 0) {
+                child[0].parentNode.replaceChild(editTextSpan, child[0]);
+                child[0].classList.toggle('hidden');
+                if (child[0].classList.contains('hidden')) {
+                    console.warn('class hidden found on' + child[0]);
+                    // child[0].classList.remove('hidden');
+                    // child[0].classList.add('show');
+                }
+            }
+        })
         let editTextSpan = document.createElement('span');
         let editTextInput = document.createElement('input');
+        let editTextArea = document.createElement('textarea');
+        let submitChange = document.createElement('button');
         editTextInput.type = 'text';
+        editTextInput.value = element.textContent;
         editTextSpan.appendChild(editTextInput);
         editTextSpan.classList.add('editText', 'hidden');
-        element.appendChild(editTextSpan);
+        submitChange.textContent = 'Save';
+        submitChange.classList.add('submitChange');
+        editTextSpan.appendChild(submitChange);
+        //check if span exists under this element:
+        let child = element.querySelectorAll('span');
+        if (child.length > 0) {
+            child[0].parentNode.replaceChild(editTextSpan, child[0]);
+        } else {
+            element.appendChild(editTextSpan);
+            // editTextSpan.classList.toggle('hidden');
+            // editTextSpan.classList.toggle('show');
+        }
         return;
     })
 }
 
+function createWorkAreaElement(elemID) {
+    let el = document.createElement(elemID);
+    el.textContent = `This is ${elemID}`;
+    workArea.appendChild(el);
+    createTextInputUnderEl(elemID);
+    // el.addEventListener('click', function (event) {
+    //     // event.preventDefault();
+    //     createTextInputUnderEl(elemID);
+    // })
+}
+
 workArea.addEventListener('drop', function (event) {
     event.preventDefault();
-
+    let workElements = this.querySelectorAll('*');
+    workElements.forEach(element => {
+        element.addEventListener('click', function (event) {
+            console.log('click on', event.target.id);
+        });
+    });
     if (this.textContent === 'This is the work area') {
         this.textContent = '';
     }
     let elemID = event.dataTransfer.getData('text');
-    if (elemID === 'h1') {
-        let element = document.createElement('h1');
-        element.textContent = 'This is H1 Heading';
-        this.appendChild(element);
-        createTextInputUnderEl();
-    } else if (elemID === 'div') {
-        const element = document.createElement('div');
-        element.textContent = 'This is a Div element';
-        this.appendChild(element);
-        createTextInputUnderEl();
-    } else if (elemID === 'span') {
-        const element = document.createElement('span');
-        element.textContent = 'This is a Span element';
-        this.appendChild(element);
-        createTextInputUnderEl();
-    } else if (elemID === 'form') {
-        const element = document.createElement('form');
-        element.textContent = 'This is a Form element';
-        this.appendChild(element);
-        createTextInputUnderEl();
-    } else if (elemID === 'p') {
-        const element = document.createElement('p');
-        element.textContent = 'This is a P element';
-        this.appendChild(element);
-        createTextInputUnderEl();
-    } else if (elemID === 'strong') {
-        const element = document.createElement('strong');
-        element.textContent = 'This is a Strong element';
-        this.appendChild(element);
-        createTextInputUnderEl();
-    } else if (elemID === 'ul') {
-        const element = document.createElement('ul');
-        element.textContent = 'This is a UL element';
-        this.appendChild(element);
-        createTextInputUnderEl();
-    } else if (elemID === 'li') {
-        const element = document.createElement('li');
-        element.textContent = 'This is a LI element';
-        this.appendChild(element);
-        createTextInputUnderEl();
-    } else if (elemID === 'img') {
-        const element = document.createElement('img');
-        element.src = 'https://picsum.photos/200/300';
-        element.alt = 'This is an IMG element';
-        element.textContent = 'This is an IMG element';
-        this.appendChild(element);
-        createTextInputUnderEl();
+    if (elemID === 'h1' || elemID === 'div' || elemID === 'span' || elemID === 'form' || elemID === 'p' || elemID === 'strong' || elemID === 'ul' || elemID === 'li' || elemID === 'img' || elemID === 'a') {
+        createWorkAreaElement(elemID);
     }
 })
